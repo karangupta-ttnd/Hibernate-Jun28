fa325a33e6e6db689e6b04fb75d9fcfcf1c5ef15
karan <karangupta.dev@gmail.com>
2017-07-07 16:46:48 +0530

    Files added

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import org.hibernate.*;
+import org.hibernate.query.Query;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.metamodel.EntityType;
+
+import java.util.Map;
+
+
+class Author {
+    String firstName, lastName;
+    int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try {
+            // 1. configuring hibernate
+            Configuration configuration = new Configuration().configure();
+
+            // 2. create sessionfactory
+            SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+            // 3. Get Session object
+            Session session = sessionFactory.openSession();
+
+            // 4. Starting Transaction
+            Transaction transaction = session.beginTransaction();
+
+
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            System.out.println("error");
+        }
+    }
+}
