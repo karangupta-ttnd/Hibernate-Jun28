d2e324fbc306392ea0f8845703b383e123987c6f
karan <karangupta.dev@gmail.com>
2017-07-09 12:34:22 +0530

    Q3-Add DOB column

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 
 @Entity
@@ -17,6 +18,8 @@ class Author {
 
     String firstName, lastName;
     int age;
+    Date DateOfBirth;
+
 
     public String getFirstName() {
         return firstName;
