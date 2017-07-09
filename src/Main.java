d892a3d1c2abd64519ace232c367dc91ca18761d
karan <karangupta.dev@gmail.com>
2017-07-10 00:24:14 +0530

    Q9-Generation type as Table

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.sql.Date;
 class Author {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     @Column(name = "Author_ID")
     int id;
 
