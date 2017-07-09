d3c0cac6e020f67c56535f8461680b05eceef230
karan <karangupta.dev@gmail.com>
2017-07-09 11:23:15 +0530

    Q2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,19 @@ import org.hibernate.*;
 import org.hibernate.query.Query;
 import org.hibernate.cfg.Configuration;
 
-import javax.persistence.metamodel.EntityType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-import java.util.Map;
 
-
+@Entity
 class Author {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    int id;
+
     String firstName, lastName;
     int age;
 
@@ -15,31 +22,34 @@ class Author {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    public Author setFirstName(String firstName) {
         this.firstName = firstName;
+        return this;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public Author setLastName(String lastName) {
         this.lastName = lastName;
+        return this;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+    public Author setAge(int age) {
         this.age = age;
+        return this;
     }
 
     @Override
     public String toString() {
         return "Author{" +
-                "firstName=" +firstName +
-                ", lastName='" + lastName + '\'' +", age='"+age+"'}";
+                "firstName='" + firstName +
+                "', lastName='" + lastName + '\'' + ", age='" + age + "'}";
     }
 
 }
@@ -60,6 +70,37 @@ public class Main {
 
             // 4. Starting Transaction
             Transaction transaction = session.beginTransaction();
+
+            Author author1 = new Author();
+            Author author2 = new Author();
+            Author author3 = new Author();
+            Author author4 = new Author();
+            author1.setFirstName("Stephen").setLastName("King").setAge(35);
+            author2.setFirstName("J.K.").setLastName("Rowling").setAge(39);
+            author3.setFirstName("Charles").setLastName("Dickens").setAge(48);
+            author4.setFirstName("William").setLastName("Shakespeare").setAge(50);
+
+            session.save(author1);
+            session.save(author2);
+            session.save(author3);
+            session.save(author4);
+
+            transaction.commit();
+
+            transaction=session.beginTransaction();
+            Author someAuthor = session.get(Author.class, 4);
+            System.out.println(someAuthor.toString());
+            someAuthor.setAge(45);
+            System.out.println(someAuthor.toString());
+            session.update(someAuthor);
+            transaction.commit();
+
+            transaction= session.beginTransaction();
+            Author someOtherAuthor = session.get(Author.class, 1);
+            session.delete(someOtherAuthor);
+            transaction.commit();
+
+            System.out.println("Done");
 
 
         } catch (HibernateException e) {
